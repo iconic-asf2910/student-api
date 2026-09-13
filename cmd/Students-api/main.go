8644ad3b8156b046f001505dfd8975d35cf29a84
Iconic <218242901+iconic-asf2910@users.noreply.github.com>
2026-09-13 12:56:35 +0530

    graceful shutdown

--- cmd/Students-api/main.go
+++ cmd/Students-api/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/iconic-asf2910/student-api/internal/config"
+)
 
 func main() {
-	fmt.Println("welcome to students api")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to student api"))
+	})
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	slog.Info("Server started", slog.String("address", cfg.HTTPServer.Addr))
+
+	fmt.Printf("server started %s", cfg.HTTPServer.Addr)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		slog.Error("failed to shut down server", slog.String("error", err.Error()))
+	}
+	slog.Info("server shut down successfully")
 }
